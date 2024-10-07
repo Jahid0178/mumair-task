1c580f7db65b8286627cf0d7d803adbe43a6f462
Jahidul <jahidulhaque767@gmail.com>
2024-10-08 01:24:36 +0600

    feat: add service section in main page

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./_components/HeroSection";
 import AboutSection from "./_components/AboutSection";
+import ServicesSection from "./_components/ServicesSection";
 
 const page = () => {
   return (
     <>
       <HeroSection />
       <AboutSection />
+      <ServicesSection />
     </>
   );
 };
