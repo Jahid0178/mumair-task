a0d251a04dc87961d88b6205ac52b7012713bff8
Jahidul <jahidulhaque767@gmail.com>
2024-10-07 22:26:36 +0600

    feat: add Header and Footer component in layout file

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Header from "@/components/layout/Header/Header";
+import Footer from "@/components/layout/Footer/Footer";
 const ClientLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
+      <Header />
       <main>{children}</main>
+      <Footer />
     </>
   );
 };
