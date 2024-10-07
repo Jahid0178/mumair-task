58ab0fa16e616c9acd2bbd592e2fee32c689324a
Jahidul <jahidulhaque767@gmail.com>
2024-10-07 22:23:37 +0600

    chore: add social links data in data.ts file

--- data/data.ts
+++ data/data.ts
@@ -1,4 +1,5 @@
-import { HeaderLinksType } from "@/typescript/types";
+import { HeaderLinksType, SocialLinkType } from "@/typescript/types";
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export const headerLinks: HeaderLinksType[] = [
   {
@@ -30,5 +31,28 @@ export const headerLinks: HeaderLinksType[] = [
     id: 6,
     name: "Contact",
     path: "/contact",
+  },
+];
+
+export const socialLinks: SocialLinkType[] = [
+  {
+    id: 1,
+    href: "#",
+    icon: FaFacebook,
+  },
+  {
+    id: 2,
+    href: "#",
+    icon: FaTwitter,
+  },
+  {
+    id: 3,
+    href: "#",
+    icon: FaInstagram,
+  },
+  {
+    id: 4,
+    href: "#",
+    icon: FaLinkedin,
   },
 ];
