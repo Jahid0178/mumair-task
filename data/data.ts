d44027ddb5640c57f774cf2637d8c6ccaf64e4a7
Jahidul <jahidulhaque767@gmail.com>
2024-10-08 01:23:12 +0600

    chore: add services data and service type

--- data/data.ts
+++ data/data.ts
@@ -1,4 +1,13 @@
-import { HeaderLinksType, SkillType, SocialLinkType } from "@/typescript/types";
+import UIUX from "@/public/images/ui-ux.png";
+import WebDesign from "@/public/images/web-design.png";
+import AppDesign from "@/public/images/app-design.png";
+import GraphicDesign from "@/public/images/graphics-design.png";
+import {
+  HeaderLinksType,
+  ServiceType,
+  SkillType,
+  SocialLinkType,
+} from "@/typescript/types";
 import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export const headerLinks: HeaderLinksType[] = [
@@ -77,5 +86,36 @@ export const skills: SkillType[] = [
     id: 4,
     name: "Graphics Design",
     percentage: 92,
+  },
+];
+
+export const services: ServiceType[] = [
+  {
+    id: 1,
+    icon: UIUX,
+    name: "UI/UX",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Morbi diam nisi nam diam interdum",
+  },
+  {
+    id: 2,
+    icon: WebDesign,
+    name: "Web Design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Morbi diam nisi nam diam interdum",
+  },
+  {
+    id: 3,
+    icon: AppDesign,
+    name: "App Design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Morbi diam nisi nam diam interdum",
+  },
+  {
+    id: 4,
+    icon: GraphicDesign,
+    name: "Graphic Design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Morbi diam nisi nam diam interdum",
   },
 ];
