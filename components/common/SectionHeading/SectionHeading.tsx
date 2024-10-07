d6773f3d42b8ee15bbb9d48ade7a8ff3f3f95cd4
Jahidul <jahidulhaque767@gmail.com>
2024-10-08 01:34:35 +0600

    fix: fixed title description size and line height issue

--- components/common/SectionHeading/SectionHeading.tsx
+++ components/common/SectionHeading/SectionHeading.tsx
@@ -13,8 +13,10 @@ const SectionHeading = ({
 }: SectionHeadingProps) => {
   return (
     <div className={`max-w-4xl w-full mx-auto space-y-4 ${className}`}>
-      <h3 className="text-5xl lg:text-6xl font-semibold">{title}</h3>
-      <p className="text-lg lg:text-xl">{description}</p>
+      <h3 className="text-5xl lg:text-[65px] font-semibold leading-[97px]">
+        {title}
+      </h3>
+      <p className="text-lg lg:text-[21px] leading-[31px]">{description}</p>
     </div>
   );
 };
