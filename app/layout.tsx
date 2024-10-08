05134884216430b268e7e18f4ef8bb4169ef36b8
Jahidul <jahidulhaque767@gmail.com>
2024-10-08 19:11:46 +0600

    chore: add import swiper pagination css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800", "900"],
