3e228cc42394371dbdd83fc1fdba492a2bbf27ec
Jahidul <jahidulhaque767@gmail.com>
2024-10-08 20:54:32 +0600

    feat: add project section in main page

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import HeroSection from "./_components/HeroSection";
 import AboutSection from "./_components/AboutSection";
 import ServicesSection from "./_components/ServicesSection";
+import ProjectsSection from "./_components/ProjectsSection";
 import TestimonialSection from "./_components/TestimonialSection";
 import CTASection from "./_components/CTASection";
 
@@ -11,6 +12,7 @@ const page = () => {
       <HeroSection />
       <AboutSection />
       <ServicesSection />
+      <ProjectsSection />
       <TestimonialSection />
       <CTASection />
     </>
